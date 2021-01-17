938bf32a5512814f3987df630bc6cf15058f6c7f
JazC <jzchan7713@gmail.com>
2021-01-16 16:34:23 -0800

    Greeting based on time; edit css

--- get_time_data.py
+++ get_time_data.py
@@ -1,11 +1,21 @@
 
-
-
 import datetime
 
-def getTime():
+
+def get_time():
+    """
+    Obtain current hour from user's system and display a greeting according to time.
+
+    :return: an appropriate greeting depending on time of day
+    """
     user_time = datetime.datetime.now()
-    return user_time
+    current_hour = user_time.hour
 
-
-
+    if current_hour in range(1, 13):
+        return "Good morning"
+    elif current_hour in range(12, 19):
+        return "Good afternoon"
+    elif current_hour in range(18, 25):
+        return "Good evening"
+    else:
+        return "Hello"
