859a93dd26a0ab51d92bb86a0db35b89f51306b6
denise <ychew2@my.bcit.ca>
2021-01-17 01:15:10 -0800

    make_playlist function

---
+++ make_playlist.py
@@ -0,0 +1,22 @@
+import json
+
+
+def make_playlist(genre1: str, genre2: str, genre3: str):
+    with open('artist_songs_dict_test.json') as json_file:
+        data = json.load(json_file)
+        playlist = []
+        genres = [genre1, genre2, genre3]
+        for artist in data:
+            track = artist["tracks"]
+            for song in track:
+                for genre in genres:
+                    if song["genreName"] == genre1:
+                        song_data = {"artistName": artist["artistName"]}
+                        song_data["trackViewUrl"] = song["trackViewUrl"]
+                        song_data["previewUrl"] = song["previewUrl"]
+                        song_data["thumbnail"] = song["thumbnail"]
+                        playlist.append(song_data)
+    return playlist
+
+
+print(make_playlist("Pop", "0", "0"))
--- pull_genres.py
+++ pull_genres.py
@@ -1,11 +1,13 @@
 import json
 
-with open('artist_songs_dict_test.json') as json_file:
-    data = json.load(json_file)
-    genrelist = []
-    for artist in data:
-        track = artist["tracks"]
-        for song in track:
-            genrelist.append(song["genreName"])
-    genreset = set(genrelist)
-    print(genreset)
+# with open('artist_songs_dict_test.json') as json_file:
+#     data = json.load(json_file)
+#     genrelist = []
+#     for artist in data:
+#         track = artist["tracks"]
+#         for song in track:
+#             genrelist.append(song["genreName"])
+#     genreset = set(genrelist)
+#     print(genreset)
+
+
