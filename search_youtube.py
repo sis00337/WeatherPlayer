fe1eacf5683e796c4f32dc820c080b81b91336e4
denise <ychew2@my.bcit.ca>
2021-01-20 00:15:23 -0800

    removed oauth. function that finds desired video data from search

--- search_youtube.py
+++ search_youtube.py
@@ -1,46 +1,45 @@
 import os
 
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
-import googleapiclient.errors
-
-scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-
-artist_name = "Taylor Swift"
-track_name = "Blank Space"
-search_query = artist_name + track_name
 
 
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
+def search_youtube(artist_name: str, track_name: str):
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "client_secret_desktop.json"
+    DEVELOPER_KEY = "AIzaSyCk8JlIji8Zj8ZHKMDI3IsZP89H-sOjRyM"
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
     request = youtube.search().list(
         part="snippet",
         maxResults=2,
-        q=search_query,
-        type="video"
+        q=artist_name + track_name
     )
     response = request.execute()
 
-    print(response)
-    search_results = response["items"]
-    first_result = search_results[0]
-    id_info = first_result["id"]
-    video_id = id_info["videoId"]
+    return response
+
+
+def find_data(search_results: dict):
+    video_id = search_results["items"][0]["id"]["videoId"]
+    video_title = search_results["items"][0]["snippet"]["title"]
+    thumbnail = search_results["items"][0]["snippet"]["thumbnails"]["medium"]["url"]
+
+    video_data = {"videoId": video_id, "videoTitle": video_title, "thumbnail": thumbnail}
     print(f"https://www.youtube.com/watch?v=" + video_id)
+    return video_data
+
+
+def main():
+    artist = "Taylor Swift"
+    song_title = "Blank Space"
+    search_results = search_youtube(artist, song_title)
+    print(search_results)
+    video_data = find_data(search_results)
+    print(video_data)
 
 
 if __name__ == "__main__":
